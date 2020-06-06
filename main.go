eed1590b0579593fe1c445115741c80ef594b9b1
bronskih <dima5202@mail.ru>
2020-06-06 23:30:21 +0500

    обновление типа данных с int32 на int64

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 func main() {
-	var balance int32 = 15_000_000_00
-	var transfer int32 = 10_000_000_00
+	balance := 15_000_000_00
+	transfer := 10_000_000_00
 	total := balance + transfer
 	println(total)
 }
